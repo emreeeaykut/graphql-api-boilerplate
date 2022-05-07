1e9c00986ff270540b969d720592f34c2d8bb0e9
emreeeaykut <emreeeaykutt@gmail.com>
2022-05-07 18:09:29 +0300

    Fix post factory

--- src/database/factories/post.factory.ts
+++ src/database/factories/post.factory.ts
@@ -5,7 +5,7 @@ import { Post } from '@entities/post.entity';
 define(Post, (f: typeof faker) => {
   const post = new Post();
 
-  const title = f.lorem.words(8);
+  const title = f.lorem.sentence();
   const description = f.lorem.sentence();
   const content = f.lorem.paragraphs();
 
